5acdb8cc7041a30cb17f58319c2a6f165fd705aa
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2024-05-28 01:13:58 -0300

    Add Get All Category Request

--- Fina.Web/obj/Debug/net8.0/Fina.Web.AssemblyInfo.cs
+++ Fina.Web/obj/Debug/net8.0/Fina.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fina.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa841eb8bc57487cf8a1c3b5b5609e4a2d226d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fina.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fina.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
