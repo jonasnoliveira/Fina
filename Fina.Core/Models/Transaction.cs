b4139f6fa50fb48f672f01a9456f6451a122599f
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2024-05-29 00:08:20 -0300

    Update Model Transaction

--- Fina.Core/Models/Transaction.cs
+++ Fina.Core/Models/Transaction.cs
@@ -11,6 +11,7 @@ public class Transaction
     public DateTime? PaidOrReceivedAt { get; set; }
 
     public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
+    public decimal Amount { get; set; }
     
     public long CategoryId { get; set; }
     public Category Category { get; set; } = null;
