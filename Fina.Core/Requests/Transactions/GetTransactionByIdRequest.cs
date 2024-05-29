0b054df955eb32616ac913442162aa60af474432
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2024-05-29 00:08:05 -0300

    Update GetTransactionByIdRequest

--- Fina.Core/Requests/Transactions/GetTransactionByIdRequest.cs
+++ Fina.Core/Requests/Transactions/GetTransactionByIdRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fina.Core.Requests.Transactions;
 
-public class GetTransactionByIdRequest
+public class GetTransactionByIdRequest : Request
 {
-    
+        public long Id { get; set; }
 }
