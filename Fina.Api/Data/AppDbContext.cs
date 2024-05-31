ecf4bee21b793e23e18686c2e2d032e3aa9ae6a7
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2024-05-31 20:25:42 -0300

    Add AppDbContext

---
+++ Fina.Api/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Fina.Api.Data.Mappings;
+using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fina.Api.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //calls one mapping/class at a time, specifies which model to use
+        //modelBuilder.ApplyConfiguration(new CategoryMapping());
+        //modelBuilder.ApplyConfiguration(new TransactionMapping());
+        
+        //or
+        
+        //calls all mappings/class that use IEntityTypeConfiguration
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
