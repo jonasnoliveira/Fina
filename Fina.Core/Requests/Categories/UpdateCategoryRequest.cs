358eb5aa2b03c66c72a4256bda13ef3132218d7c
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2024-05-31 20:27:48 -0300

    Update UpdateCategoryRequest

--- Fina.Core/Requests/Categories/UpdateCategoryRequest.cs
+++ Fina.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -5,10 +5,10 @@ namespace Fina.Core.Requests.Categories;
 public class UpdateCategoryRequest : Request
 {
     public long Id { get; set; }
-    
-    [Required (ErrorMessage = "Título é obrigatório")]
+
+    [Required(ErrorMessage = "Título é obrigatório")]
     [MaxLength(80, ErrorMessage = "Título deve ter no maximo 80 caracteres")]
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
     
     [MaxLength(255, ErrorMessage = "Descricão deve ter no maximo 255 caracteres")]
     public string? Description { get; set; }
